e3ed7ec2a6349e448e4e79f47ad96900dac3bb87
jennifeflournoy@gmail.com <student@InnovateBhams-MacBook-Pro-7.local>
2020-02-04 10:25:25 -0600

    first commit

---
+++ ajs.js
@@ -0,0 +1,50 @@
+let fruits = ['apple', 'banana', 'grapes'];
+let favFruit = fruits[2];
+let leastFav = 'banana'[1];
+function printFruits() {
+    let favFruit = fruits[2];
+    console.log(fruits[0])
+
+function printFavFruit(){
+    console.log(favFruit);
+    printFruits();
+    printFavFruit();
+}
+}
+
+
+
+function printleastFav(){
+    console.log(leasFav);
+}
+
+
+someFunc();
+function someFunc(){
+    console.log("Hello Jennifer");
+
+}
+
+
+function myFunc() {
+    alert ("Happy New Year");
+}
+ myFunc();
+
+
+
+
+// console.log('Wine');
+// let avg = findAvg(2,2);
+// console.log('Popcorn, avg');
+// function findAvg(a,b){
+//     console.log('Relaxation');
+//     var answer = ( a + b) / 2;
+//     return answer;
+// }
+
+// function setName(){
+
+// var name = 'Convalence';
+// console.log(name);
+// }
